5236bdffeced9aba943d28e35c5de2f3f67f7895
ashbrick <ashleycbrickhouse@gmail.com>
2019-11-22 11:26:40 -0500

    changed workout schema so that it goes with the exercise schema so I commented out sets, reps and rest...

--- models/workouts.js
+++ models/workouts.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const workoutSchema = new mongoose.Schema({
     type: String,
     duration: String,
-    sets: Number,
-    reps: Number,
-    rest: Number
+    date: String
+    //commenting these out because I created another schema for exercises in order to relate the two dbs (exercises and workouts)
+    // duration: String,
+    // sets: Number,
+    // reps: Number,
+    // rest: Number
 });
 
 const Workouts = mongoose.model('Workout', workoutSchema);
